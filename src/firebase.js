e7ecc73ebaaf4ac1151ae932452ca5191aff1089
Glory Onyeka Arisah <gloryarisah@Glorys-MacBook-Pro.local>
2022-10-15 17:50:01 +0100

    Created user authentication functionalities Setup firebase deployment

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,14 @@
 import { initializeApp } from 'firebase/app'
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  sendPasswordResetEmail,
+  signOut,
+  GoogleAuthProvider
+} from 'firebase/auth'
+import { getFireStore, addDoc, collection, query, getDocs } from 'firebase/firestore'
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
@@ -11,3 +21,63 @@ const firebaseConfig = {
 }
 
 const app = initializeApp(firebaseConfig)
+const db = getFireStore()
+const auth = getAuth()
+
+const googleProvider = new GoogleAuthProvider()
+
+const signInWithGoogle = async () => {
+  try {
+    const resp = await signInWithPopup(auth, googleProvider)
+    const user = resp.user
+    const q = query(collection(db, 'users'), where('uid', '==', user.uid))
+    const docs = await getDocs(q)
+    console.log(q)
+    // check if user exists in cloud firestore
+    if (docs.docs.length === 0) {
+      await addDoc(collection(db, 'users'), {
+        id: user.id,
+        name: user.displayName,
+        email: user.email,
+        authProvider: 'google'
+      })
+    }
+  } catch (error) {
+    alert(error.message)
+  }
+}
+
+const logInWithEmailAndPassword = async (email, password) => {
+  try {
+    await signInWithEmailAndPassword(auth, email, password)
+  } catch (error) {
+    alert(error.message)
+  }
+}
+
+const registerWithEmailAndPassword = async (displayName, email, password) => {
+  try {
+    const resp = await createUserWithEmailAndPassword(auth, email, password)
+    const user = resp.user
+    await addDoc(collection(db, 'users'), {
+      id: user.id,
+      email,
+      displayName,
+      authProvider: 'emailAndPassword'
+    })
+  } catch (error) {
+    
+  }
+}
+
+const sendPasswordReset = async (email) => {
+  try {
+    await sendPasswordResetEmail(auth, email)
+  } catch (error) {
+    alert(error.message)
+  }
+}
+
+const logout = async () => {
+  await signOut(auth)
+}
